d7906efe13b1a2457567426c633e59b4d0ed1b7e
DoubleHong <greathongtian@gmail.com>
2023-01-25 17:02:38 +0800

    Hong

--- src/main/java/com/example/training_platform_h/controller/OrganizationInfoController.java
+++ src/main/java/com/example/training_platform_h/controller/OrganizationInfoController.java
@@ -2,12 +2,10 @@ package com.example.training_platform_h.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.training_platform_h.entity.PersonalInfoEntity;
+import com.example.training_platform_h.mapper.OrganizationInfoMapper;
 import com.example.training_platform_h.mapper.PersonalInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,9 @@ public class OrganizationInfoController {
     @Autowired
     PersonalInfoMapper personalInfoMapper;
 
+    @Autowired
+    OrganizationInfoMapper organizationInfoMapper;
+
     @PostMapping("/checkUser")//用户登录检查密码是否正确
     public String checkUser(@RequestBody PersonalInfoEntity personalInfo) {
         System.out.println(personalInfo);
@@ -38,4 +39,11 @@ public class OrganizationInfoController {
 
     }
 
+    @GetMapping("/getNameById/{id}")//根据机构ID获取机构名
+    public String getNameById(@PathVariable String id){
+        System.out.println(id);
+        System.out.println(organizationInfoMapper.selectById(id));
+        System.out.println("ssss");
+        return organizationInfoMapper.selectById(id).getName();
+    }
 }
--- src/main/java/com/example/training_platform_h/controller/PersonalInfoController.java
+++ src/main/java/com/example/training_platform_h/controller/PersonalInfoController.java
@@ -29,53 +29,60 @@ public class PersonalInfoController {
     public int checkUser(@RequestBody PersonalInfoEntity personalInfo) {
         System.out.println(personalInfo);
         List<PersonalInfoEntity> lists = personalInfoMapper.selectList(new QueryWrapper<PersonalInfoEntity>()
-                .eq("username", personalInfo.getUsername()).eq("password", personalInfo.getPassword()).eq("user_type",personalInfo.getUserType()));
+                .eq("username", personalInfo.getUsername()).eq("password", personalInfo.getPassword()).eq("user_type", personalInfo.getUserType()));
 
         if (lists.size() != 0) {
             return 1;
-        } else{
+        } else {
             return 0;
         }
 
     }
-  @PostMapping("/zhuceUser")//注册用户
-    public int zhuceUser(@RequestBody PersonalInfoEntity personinfo){
-      UUID uuid = UUID.randomUUID();
-      String id =uuid.toString();
-      personinfo.setId(id);
-      personinfo.setOrganizationId(null);
-      if (personinfo.getUsername()==null||personinfo.getPassword()==null){
-          return -1;
-      }
-      return personalInfoMapper.insert(personinfo);
-  }
 
-  @GetMapping("/getPersonalInfo/{id}")
-    public PersonalInfoEntity getPersonalInfo(@PathVariable String id){
-        return personalInfoMapper.selectById(id);
-  }
-
-  @PostMapping("/checkOldPassword")//修改密码检查原密码是否正确
-    public int checkOldPassword(@RequestBody PersonalInfoEntity personalInfo){
-        List<PersonalInfoEntity> lists = personalInfoMapper.selectList(new QueryWrapper<PersonalInfoEntity>().eq("id",personalInfo.getId())
-                .eq("password",personalInfo.getPassword()));
-        if (lists.size()!=0){
-            return 1;
+    @PostMapping("/zhuceUser")//注册用户
+    public int zhuceUser(@RequestBody PersonalInfoEntity personinfo) {
+        UUID uuid = UUID.randomUUID();
+        String id = uuid.toString();
+        personinfo.setId(id);
+        personinfo.setOrganizationId(null);
+        if (personinfo.getUsername() == null || personinfo.getPassword() == null) {
+            return -1;
         }
-        else {
+        return personalInfoMapper.insert(personinfo);
+    }
+
+    @GetMapping("/getPersonalInfo/{id}")
+    public PersonalInfoEntity getPersonalInfo(@PathVariable String id) {
+        return personalInfoMapper.selectById(id);
+    }
+
+    @PostMapping("/checkOldPassword")//修改密码检查原密码是否正确
+    public int checkOldPassword(@RequestBody PersonalInfoEntity personalInfo) {
+        List<PersonalInfoEntity> lists = personalInfoMapper.selectList(new QueryWrapper<PersonalInfoEntity>().eq("id", personalInfo.getId())
+                .eq("password", personalInfo.getPassword()));
+        if (lists.size() != 0) {
+            return 1;
+        } else {
             return 0;
         }
-  }
-  @PostMapping("/modifyPassword")//修改密码
-    public int modifyPassword(@RequestBody PersonalInfoEntity personalInfo){
+    }
+
+    @PostMapping("/modifyPassword")//修改密码
+    public int modifyPassword(@RequestBody PersonalInfoEntity personalInfo) {
         PersonalInfoEntity modify = personalInfoMapper.selectById(personalInfo.getId());
         modify.setPassword(personalInfo.getPassword());
         return personalInfoMapper.updateById(modify);
-  }
-    @GetMapping("/FindUserByUserName/{username}")//用username找出全部user信息
-    public PersonalInfoEntity FindUserByUserName(@PathVariable String username){
-        return personalInfoMapper.selectOne(Wrappers.<PersonalInfoEntity>lambdaQuery().eq(PersonalInfoEntity::getUsername,username));
+    }
 
+    @GetMapping("/FindUserByUserName/{username}")//用username找出全部user信息
+    public PersonalInfoEntity FindUserByUserName(@PathVariable String username) {
+        return personalInfoMapper.selectOne(Wrappers.<PersonalInfoEntity>lambdaQuery().eq(PersonalInfoEntity::getUsername, username));
+
+    }
+
+    @PostMapping("/modifyInfoById")//机构管理员修改个人信息
+    public int modifyInfoById(@RequestBody PersonalInfoEntity personalInfo){
+        return personalInfoMapper.updateById(personalInfo);
     }
 
 }
